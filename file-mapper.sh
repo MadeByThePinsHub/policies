e7769e5b29f9e9e1023974b6c5fab9f390cf203c
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-04-24 12:56:45 +0800

    Create file-mapper.sh

---
+++ file-mapper.sh
@@ -0,0 +1,8 @@
+#!/bin/env sh
+
+PROJECT_ROOT=${PROJECT_ROOT:/home/runner/todo}
+
+cd $PROJECT_ROOT/handbook-src/community-hub/oss-policies
+mv README.md index.md
+mv CODE_OF_CONDUCT.md code-of-conduct.md
+mv SECURITY.md security.md
