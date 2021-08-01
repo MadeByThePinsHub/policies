14768af5a94f9d70039b8b8dcdcc75ce99287216
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-08-01 14:17:23 +0800

    chore: pull tarball instead of copying stuff
    I'm not sure of its works, but YOLO.

--- file-mapper.sh
+++ file-mapper.sh
@@ -1,10 +1,33 @@
-#!/bin/env sh
+#!/bin/bash
 
-PROJECT_ROOT=${PROJECT_ROOT:/home/runner/todo}
+##########################################################################################
+# WARNING: THIS SCRIPT IS IN PROGRESS OF REWRITING, DO NOT USE THIS ON THE BUILD STEP!   #
+##########################################################################################
+# Once this script has been tested and it worked, you can safely remove this notice btw. #
+##########################################################################################
 
-cd $PROJECT_ROOT/handbook-src/community-hub/oss-policies
-mv README.md index.md
-mv CODE_OF_CONDUCT.md code-of-conduct.md
-mv SECURITY.md security.md
-rm -rf .github
-cd $OLDPWD
+set -xe
+
+# The File Mapping script should be ran during the build step of our handbook site. Note that you must
+# use the curl | sh way or wget this file and execute it. You may also customize this script to use your own
+# policies repo on your handbook website.
+
+PROJECT_ROOT=${PROJECT_ROOT:$PWD}
+
+# Adjust these to use your own mirrors on your own Git servers
+DISABLE_SSL=false # when set to true, we'll use 
+GIT_HOST=github.com
+GIT_REPO_PATH=MadeByThePinsHub/policies
+GIT_BRANCH="master"
+TARBALL_GENERATOR_PATH=tarball
+
+if [[ $DISABLE_SSL == "true" ]]; then
+  wget -qO- "http://$GIT_HOST/$GIT_REPO_PATH/$TARBALL_GENERATOR_PATH/$GIT_BRANCH" | tar -xJv
+else
+  wget -qO- "https://$GIT_HOST/$GIT_REPO_PATH/$TARBALL_GENERATOR_PATH/$GIT_BRANCH" | tar -xJv
+fi
+
+if [[ -d "$PROJECT_ROOT/handbook-src/community-hub/oss-policies" ]; then
+  true
+fi
+  
